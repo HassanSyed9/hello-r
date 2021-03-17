4e19b80000b6c49128eaeaab0307c4578bcba0b3
Hassan Syed <hassansyedgithub@gmail.com>
2021-03-16 22:36:12 -0400

    Few Lines added

--- testing.r
+++ testing.r
@@ -7,3 +7,7 @@ version = "RStudio - In Writing"
 print(name)
 print("These lines has been written in RStudio")
 
+AbuBakar = "Frist Caliph of Islam"
+
+print(AbuBakar)
+print("very loyal friend of Prophet Mohammad PBUH")
